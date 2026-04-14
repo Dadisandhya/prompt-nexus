56bd6cbb7deedcbe00d83c5b3fe1eca11878e558
Dadi Sandhya <150679402+Dadisandhya@users.noreply.github.com>
2026-04-14 20:50:30 +0530

    Add home route to urlpatterns in urls.py

--- backend/prompts/urls.py
+++ backend/prompts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home),
     path('prompts/', views.get_prompts),
     path('prompts/create/', views.create_prompt),
     path('prompts/<uuid:id>/', views.get_prompt),
 ]
-# path('', views.home),
